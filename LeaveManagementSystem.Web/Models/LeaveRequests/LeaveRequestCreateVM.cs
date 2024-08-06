fd5f7791b4f1215e14e293b3075c5c65b17520e9
kev <kev@daryus.com>
2024-08-06 13:36:44 +0100

    wip...145

--- LeaveManagementSystem.Web/Models/LeaveRequests/LeaveRequestCreateVM.cs
+++ LeaveManagementSystem.Web/Models/LeaveRequests/LeaveRequestCreateVM.cs
@@ -1,10 +1,18 @@
-﻿namespace LeaveManagementSystem.Web.Models.LeaveRequests
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Models.LeaveRequests
 {
     public class LeaveRequestCreateVM
     {
+        [Display(Name = "Start Date")]
         public DateOnly StartDate { get; set; }
+        [Display(Name = "End Date")]
         public DateOnly EndDate { get; set; }
+        [Display(Name = "Leave Type")]
         public int LeaveTypeId { get; set; }
+        [Display(Name = "Additional Information")]
         public string? RequestComments { get; set; }
+        public SelectList LeaveTypes { get; set; }
     }
 }
