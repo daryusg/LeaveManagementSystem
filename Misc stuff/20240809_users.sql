42a1a2b4ffe015adcc62f2f9cf427f330c063a56
daryusg <kev@daryus.com>
2024-08-12 09:00:49 +0100

    Delete Misc stuff directory

--- Misc stuff/20240809_users.sql
+++
@@ -1,4 +0,0 @@
-﻿select u.email, concat(u.FirstName, ' ', u.LastName) as Name , r.Name as role
-from AspNetRoles r inner join AspNetUserRoles ur on r.id = ur.RoleId
-	inner join AspNetUsers u on u.Id = ur.UserId
-order by email
